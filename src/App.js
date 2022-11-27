93e4ad09315793955fd73b79bd965367ef398fe7
Yusuf Said SAYIN <yusuff6752@gmail.com>
2022-11-28 00:19:11 +0300

    page title design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Pricing from './pages/Pricing/Pricing'
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Pricing></Pricing>
     </div>
   )
 }
---
+++ src/pages/Pricing/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader/PageHeader'
+
+const Pricing = () => {
+  return (
+   <>
+   <PageHeader title='Pricing Plans' text="We are an industry-leading company that values honesty, integrity, and efficiency. Building quality products and caring."></PageHeader>
+   </>
+  )
+}
+
+export default Pricing
